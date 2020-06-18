ec16b30ef3de63864fcc4c6e40a8d22a8962664a
Nicolò Tumino <64739080+Snjch@users.noreply.github.com>
2020-06-18 13:14:42 +0200

    Delete RegRemoteInterface.java

--- RegRemoteInterface.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RegRemoteInterface extends Remote {
-    public int userRegistration(String nickname, String passwd) throws RemoteException, NullPointerException;
-}
